2ce016a031a0b523bef0f2514719317ef15040b3
Rosana Cardozo <rosanacardozo07@gmail.com>
2018-11-02 14:00:30 -0400

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,355 @@
+import React, { Component } from "react";
+import LocationList from "./LocationList";
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      alllocations: require("./places.json"), 
+      infowindow: "",
+      prevmarker: ""
+    };
+
+    // retain object 
+    this.initMap = this.initMap.bind(this);
+    this.openInfoWindow = this.openInfoWindow.bind(this);
+    this.closeInfoWindow = this.closeInfoWindow.bind(this);
+  }
+
+  componentDidMount() {
+    // Connect the initMap() 
+    window.initMap = this.initMap;
+    
+    loadMapJS(
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyCacmIGcWsolGnXLp71cBM_My9axyprocM&callback=initMap"
+    );
+  }
+
+  
+   // Initialise the map 
+  
+  initMap() {
+    var self = this;
+
+//  referenced map style  from snazzy maps
+    var styles =[
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "lightness": 13
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#144b53"
+            },
+            {
+                "lightness": 14
+            },
+            {
+                "weight": 1.4
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.locality",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#08304b"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#0c4152"
+            },
+            {
+                "lightness": 5
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#0b434f"
+            },
+            {
+                "lightness": 25
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "color": "#0b3d51"
+            },
+            {
+                "lightness": 16
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#146474"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#021019"
+            }
+        ]
+    }
+]
+
+
+    var mapview = document.getElementById("map");
+    mapview.style.height = window.innerHeight + "px";
+    var map = new window.google.maps.Map(mapview, {
+      center: { lat: 25.801412, lng: -80.199087},
+      zoom: 14,
+      styles: styles,
+      mapTypeControl: false
+    });
+
+    var InfoWindow = new window.google.maps.InfoWindow({});
+
+    window.google.maps.event.addListener(InfoWindow, "closeclick", function() {
+      self.closeInfoWindow();
+    });
+
+    this.setState({
+      map: map,
+      infowindow: InfoWindow
+    });
+
+    window.google.maps.event.addDomListener(window, "resize", function() {
+      var center = map.getCenter();
+      window.google.maps.event.trigger(map, "resize");
+      self.state.map.setCenter(center);
+    });
+
+    window.google.maps.event.addListener(map, "click", function() {
+      self.closeInfoWindow();
+    });
+
+    var alllocations = [];
+    this.state.alllocations.forEach(function(location) {
+      var longname = location.name + " - " + location.type;
+      var marker = new window.google.maps.Marker({
+        position: new window.google.maps.LatLng(
+          location.latitude,
+          location.longitude
+        ),
+        animation: window.google.maps.Animation.DROP,
+        map: map
+      });
+
+      marker.addListener("click", function() {
+        self.openInfoWindow(marker);
+      });
+
+      location.longname = longname;
+      location.marker = marker;
+      location.display = true;
+      alllocations.push(location);
+    });
+    this.setState({
+      alllocations: alllocations
+    });
+  }
+
+
+    // Open the infowindow 
+   
+ 
+  openInfoWindow(marker) {
+    this.closeInfoWindow();
+    this.state.infowindow.open(this.state.map, marker);
+    marker.setAnimation(window.google.maps.Animation.BOUNCE);
+    this.setState({
+      prevmarker: marker
+    });
+    this.state.infowindow.setContent("Loading Data...");
+    this.state.map.setCenter(marker.getPosition());
+    this.state.map.panBy(0, -200);
+    this.getMarkerInfo(marker);
+  }
+
+  // Foursquare info
+  getMarkerInfo(marker) {
+    var self = this;
+
+    var clientId = "Q5MK2XFDK3FVTDQLOQKSFTKS1CI1XEWZSO2TIPP5DU2PWICK";
+    var clientSecret = "MQ3CZLR5KY1F04FUAX5YWXOLYRRJSYFWCHZANZZ23M4WI05L";
+
+    var url =
+      "https://api.foursquare.com/v2/venues/search?client_id=" +
+      clientId +
+      "&client_secret=" +
+      clientSecret +
+      "&v=20130815&ll=" +
+      marker.getPosition().lat() +
+      "," +
+      marker.getPosition().lng() +
+      "&limit=1";
+    fetch(url)
+      .then(function(response) {
+        if (response.status !== 200) {
+          self.state.infowindow.setContent("Sorry data can't be loaded");
+          return;
+        }
+
+        response.json().then(function(data) {
+          console.log(data);
+
+          var location_data = data.response.venues[0];
+          var place = `<h3>${location_data.name}</h3>`;
+          var street = `<p>${location_data.location.formattedAddress[0]}</p>`;
+          var contact = "";
+          if (location_data.contact.phone)
+            contact = `<p><small>${location_data.contact.phone}</small></p>`;
+          var checkinsCount =
+            "<b>Number of CheckIn: </b>" +
+            location_data.stats.checkinsCount +
+            "<br>";
+          var readMore =
+            '<a href="https://foursquare.com/v/' +
+            location_data.id +
+            '" target="_blank">Read More on <b>Foursquare Website</b></a>';
+          self.state.infowindow.setContent(
+            place + street + contact + checkinsCount + readMore
+          );
+        });
+      })
+      .catch(function(err) {
+        self.state.infowindow.setContent("Sorry data can't be loaded");
+      });
+  }
+
+  closeInfoWindow() {
+    if (this.state.prevmarker) {
+      this.state.prevmarker.setAnimation(null);
+    }
+    this.setState({
+      prevmarker: ""
+    });
+    this.state.infowindow.close();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <LocationList
+          key="100"
+          alllocations={this.state.alllocations}
+          openInfoWindow={this.openInfoWindow}
+          closeInfoWindow={this.closeInfoWindow}
+        />
+        <div id="map" />
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
+function loadMapJS(src) {
+  var ref = window.document.getElementsByTagName("script")[0];
+  var script = window.document.createElement("script");
+  script.src = src;
+  script.async = true;
+  script.onerror = function() {
+    document.write("Google Maps can't be loaded");
+  };
+  ref.parentNode.insertBefore(script, ref);
+}
